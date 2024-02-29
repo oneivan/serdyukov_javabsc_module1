bb30e2ef50f254892424b3467252697be3717646
oneivan <enekbenek@gmail.com>
2024-02-29 20:31:39 +0300

    init hw4

---
+++ src/main/java/sia/javabsc/User.java
@@ -0,0 +1,9 @@
+package sia.javabsc;
+
+public class User {
+    private String f_name;
+    private String i_name;
+    private String o_name;
+    private int bday_yy;
+    private String email;
+}
