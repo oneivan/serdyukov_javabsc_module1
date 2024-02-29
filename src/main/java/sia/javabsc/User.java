877b6f3cc80493063356b124cb5f42dbf1ea11ce
Ivan Serdyukov <oneivan@MacBook-Pro-Ivan.local>
2024-02-29 21:24:20 +0300

    add User Class with usage

--- src/main/java/sia/javabsc/User.java
+++ src/main/java/sia/javabsc/User.java
@@ -6,4 +6,19 @@ public class User {
     private String o_name;
     private int bday_yy;
     private String email;
+
+    public User( String f_name, String i_name, String o_name, int bday_yy, String email ) {
+        this.f_name = f_name;
+        this.i_name = i_name;
+        this.o_name = o_name;
+        this.bday_yy = bday_yy;
+        this.email = email;
+    }
+
+    public void getInfo() {
+        System.out.printf( "ФИО: %s %s %s\n", this.f_name, this.i_name, this.o_name );
+        System.out.println("Год рождения: " + this.bday_yy );
+        System.out.println("e-mail: : " + this.email );
+    }
+    public int getBDY() { return  this.bday_yy; }
 }
