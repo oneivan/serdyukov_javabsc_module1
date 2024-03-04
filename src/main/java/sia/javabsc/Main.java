195cb0b06758e1025f0e5d39bcf70294c1822642
Ivan Serdyukov <oneivan@MacBook-Pro-Ivan.local>
2024-03-04 20:44:14 +0300

    HW6 , lesson 11 - ready for PR

--- src/main/java/sia/javabsc/Main.java
+++ src/main/java/sia/javabsc/Main.java
@@ -2,6 +2,17 @@ package sia.javabsc;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Plate p = new Plate(30);
+        Cat[] cats = {
+                new Cat("Barsik", 11), new Cat("Sema", 15)
+                , new Cat("Gans", 6), new Cat("Landay", 2), new Cat("Livshic", 22)
+        };
+        for (int i = 0; i < cats.length; i++) {
+            cats[i].Eat(p);
+        }
+        for (int i = 0; i < cats.length; i++) {
+            cats[i].showInfo();
+        }
+        System.out.println("Несъедено " + p.getCrntAmt());
     }
 }
